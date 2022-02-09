6eacbe4835ceb91bcbd3a8567b3ac09abb2604e8
Jonathant4242 <91383892+Jonathant4242@users.noreply.github.com>
2022-02-08 21:23:30 -0800

    director complete

--- gameClasses/director.py
+++ gameClasses/director.py
@@ -1,16 +1,48 @@
-class director:
-    
-"""
-attrubites:
+from guess import Guess, GameStatus
+from picture import Picture
+
+"""attrubites:
     picture
-    random_word
     guess
 
 methods:
     start game
     _take_turn
     _display_terminal_output
-    __int__
+    __int__"""
+
+class director:
+    
+    def __init__(self) -> None:
+        self.guess = Guess()
+        self.picture = Picture()
+        self._game_status = GameStatus.PLAY()
+    
+
+    def start_game(self):
+        self.guess.set_word()
+        while True:
+            self._display_terminal_output()
+            self._take_turn
+            if self._game_status != GameStatus.PLAY:
+                break
+        self._display_terminal_output()
+        if self._game_status == GameStatus.WIN:
+            print('You successfully landed your jump!')
+        else:
+            print('You had a rough landing. Ambulance is on the way!')
 
 
-"""
+    def _take_turn(self):
+        self.guess.make_guess()
+        self._game_status = self.guess.get_game_status()
+
+    def _display_terminal_output(self):
+        self.guess.display_puzzle()
+        print("")
+        self.picture.draw_jumper(self.guess.get_num_wrong(), self._game_status)
+
+
+        
+
+
